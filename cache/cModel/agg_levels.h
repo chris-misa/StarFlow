c187946ca939df80860bff50e9cb7acdb0504d50
Chris Misa <cmisa@cs.uoregon.edu>
2020-09-16 03:02:38 -0700

    Added agg levels for tasks experiment

--- cache/cModel/agg_levels.h
+++ cache/cModel/agg_levels.h
@@ -27,6 +27,7 @@ void setKey_ORIGINAL(char *keyBuf, const struct ip* ipHeader, const struct udphd
   memcpy(&(keyBuf[12]), &ipHeader->ip_p, 1);
 }
 
+/* This aggregation level is for the Poisson-distributed queries --- each query rate saturates the possible aggregations so there's just one */
 void setKey_saturatedWorkload(char *keyBuf, const struct ip* ipHeader, const struct udphdr* udpOrtcpHeader){
   uint32_t src = ipHeader->ip_src.s_addr & htonl(0xFFFF0000);
   uint32_t dst = ipHeader->ip_dst.s_addr & htonl(0xFFFF0000);
@@ -40,9 +41,55 @@ void setKey_saturatedWorkload(char *keyBuf, const struct ip* ipHeader, const str
   }
 }
 
-#define NUM_KEYGENS 1
+
+/*
+ * Agg levels used in multiple-MRT heavy hitters tasks experiment
+ * The mask numbers were generated with switch-module/evaluation/starflow/gen_agg_levels.py
+ */
+
+/* 1 task */
+void setKey_tasks1(char *keyBuf, const struct ip* ipHeader, const struct udphdr* udpOrtcpHeader){
+  uint32_t dst = ipHeader->ip_dst.s_addr & htonl(0xFFE00000);
+  memcpy(&(keyBuf[4]), &dst, 4);
+}
+
+/* 2 tasks */
+void setKey_tasks2(char *keyBuf, const struct ip* ipHeader, const struct udphdr* udpOrtcpHeader){
+  uint32_t src = ipHeader->ip_src.s_addr & htonl(0xFF800000);
+  memcpy(&(keyBuf[0]), &src, 4);
+}
+
+/* 3 tasks */
+void setKey_tasks3(char *keyBuf, const struct ip* ipHeader, const struct udphdr* udpOrtcpHeader){
+  uint32_t src = ipHeader->ip_src.s_addr & htonl(0xFFF80000);
+  uint32_t dst = ipHeader->ip_dst.s_addr & htonl(0xF8000000);
+  memcpy(&(keyBuf[0]), &src, 4);
+  memcpy(&(keyBuf[4]), &dst, 4);
+}
+
+/* 4 tasks */
+void setKey_tasks4(char *keyBuf, const struct ip* ipHeader, const struct udphdr* udpOrtcpHeader){
+  uint32_t src = ipHeader->ip_src.s_addr & htonl(0xFFFC0000);
+  uint32_t dst = ipHeader->ip_dst.s_addr & htonl(0xFC000000);
+  memcpy(&(keyBuf[0]), &src, 4);
+  memcpy(&(keyBuf[4]), &dst, 4);
+}
+
+/* 5 tasks */
+void setKey_tasks5(char *keyBuf, const struct ip* ipHeader, const struct udphdr* udpOrtcpHeader){
+  uint32_t src = ipHeader->ip_src.s_addr & htonl(0xFFE00000);
+  uint32_t dst = ipHeader->ip_dst.s_addr & htonl(0xFFC00000);
+  memcpy(&(keyBuf[0]), &src, 4);
+  memcpy(&(keyBuf[4]), &dst, 4);
+}
+
+#define NUM_KEYGENS 5
 starflow_setKey_t key_gens[NUM_KEYGENS] = {
-    setKey_saturatedWorkload,
+    setKey_tasks1,
+    setKey_tasks2,
+    setKey_tasks3,
+    setKey_tasks4,
+    setKey_tasks5,
 };
 
 int cur_key_gen = 0;
